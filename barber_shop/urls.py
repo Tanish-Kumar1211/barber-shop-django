fc78b2475d94613cd63cccd49442e0a77db4cc45
Tanish-Kumar1211 <tanishrana1313@gmail.com>
2025-11-15 11:18:52 +0530

    Removed browser-reload for production

--- barber_shop/urls.py
+++ barber_shop/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('barber.urls')), 
 ]
 
